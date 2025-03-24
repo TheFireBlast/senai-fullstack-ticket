a5b8b50d68f170eedbfaa7776fcb195cfcf76106
TheFireBlast <fireblast901@gmail.com>
2025-03-24 14:15:02 -0300

    melhorar seletor de avatar

--- main.js
+++ main.js
@@ -2,20 +2,22 @@ const form = document.querySelector("form");
 /** @type {HTMLInputElement} */
 const avatarInput = document.getElementById("avatar");
 
+/**@type {string | ArrayBuffer | null} */
+let avatar = null;
+
+function removeAvatar() {
+	avatarInput.value = null;
+	avatarInput.dispatchEvent(new Event("change"));
+}
+
 form.addEventListener("submit", (ev) => {
 	ev.preventDefault();
 
-	const reader = new FileReader();
-	reader.onload = (ev) => {
-		document
-			.querySelectorAll(".data-avatar")
-			.forEach((e) => (e.src = ev.target.result));
-	};
-	reader.readAsDataURL(avatarInput.files[0]);
+	document.querySelectorAll(".data-avatar").forEach((e) => (e.src = avatar));
 
 	const name = document.getElementById("fullname").value;
 	const email = document.getElementById("email").value;
-	const username = (document.getElementById("username").value = "@jaoferr");
+	const username = document.getElementById("username").value;
 	document
 		.querySelectorAll(".data-name")
 		.forEach((e) => (e.textContent = name));
@@ -49,12 +51,32 @@ form.addEventListener("submit", (ev) => {
 	document.getElementById("result").style.display = "initial";
 });
 
+avatarInput.addEventListener("change", () => {
+	if (avatarInput.files && avatarInput.files.length > 0) {
+		document.querySelector("#avatar-wrapper .empty").style.display = "none";
+		document.querySelector("#avatar-wrapper .filled").style.display =
+			"contents";
+		const reader = new FileReader();
+		reader.onload = (ev) => {
+			avatar = ev.target.result;
+			document.querySelector("#avatar-wrapper .filled img").src = avatar;
+		};
+		reader.readAsDataURL(avatarInput.files[0]);
+	} else {
+		document.querySelector("#avatar-wrapper .empty").style.display =
+			"contents";
+		document.querySelector("#avatar-wrapper .filled").style.display =
+			"none";
+	}
+});
+
 document.getElementById("devfill").onclick = async () => {
 	const blob = await (await fetch("assets/images/image-avatar.jpg")).blob();
 	let file = new File([blob], "avatar.png");
 	let container = new DataTransfer();
 	container.items.add(file);
 	avatarInput.files = container.files;
+	avatarInput.dispatchEvent(new Event("change"));
 	document.getElementById("fullname").value = "João Ferreira";
 	document.getElementById("email").value = "joaoferreira@gmail.com";
 	document.getElementById("username").value = "@jaoferr";
